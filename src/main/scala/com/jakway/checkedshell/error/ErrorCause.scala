1da71aa5d490b6e5ae3db16a307bc069b9f22d3d
Thomas Jakway <tjakway@nyu.edu>
2019-08-17 10:53:14 -0400

    implemented Job.run, added abstract doRun

--- src/main/scala/com/jakway/checkedshell/error/ErrorCause.scala
+++ src/main/scala/com/jakway/checkedshell/error/ErrorCause.scala
@@ -4,6 +4,24 @@ import com.jakway.checkedshell.data.ProgramOutput
 
 trait ErrorCause
 
+object ErrorCause {
+  /**
+   * return or wrap the errors
+   * @param xs
+   * @return
+   */
+  def apply(xs: Set[ErrorCause]): ErrorCause = {
+    if(xs.size == 1) {
+      xs.head
+    } else {
+      MultipleErrors(xs)
+    }
+  }
+}
+
+case class MultipleErrors(errors: Set[ErrorCause])
+  extends ErrorCause
+
 case class UnhandledException(t: Throwable)
   extends ErrorCause
 
--- src/main/scala/com/jakway/checkedshell/process/Job.scala
+++ src/main/scala/com/jakway/checkedshell/process/Job.scala
@@ -2,14 +2,52 @@ package com.jakway.checkedshell.process
 
 import com.jakway.checkedshell.config.RunConfiguration
 import com.jakway.checkedshell.data.ProgramOutput
-import com.jakway.checkedshell.process.Job.JobOutput
+import com.jakway.checkedshell.error.{ErrorCause, ErrorData}
+import com.jakway.checkedshell.process.Job.{CheckFunction, JobOutput}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait Job {
-  def run(input: Option[ProgramOutput])(implicit runConfiguration: RunConfiguration): JobOutput
+  final def run(input: Option[ProgramOutput])
+               (implicit runConfiguration: RunConfiguration,
+                         ec: ExecutionContext): JobOutput = {
+
+    doRun(input)
+      .map { output =>
+
+        val errs = checks.foldLeft(Set.empty: Set[ErrorCause]) {
+          case (acc, thisCheck) => {
+            acc ++ thisCheck(output).toSet
+          }
+        }
+        if(errs.isEmpty) {
+          output
+        } else {
+          val finalCause = ErrorCause(errs)
+
+          val errorData = ErrorData(None, finalCause)
+
+          handleErrors(errorData, runConfiguration)
+
+          //depending on the run configuration the error will either have caused execution to
+          //terminate or will have been handled somehow
+          //return the output if we're going to continue
+          output
+        }
+      }
+  }
+
+  private def handleErrors(e: ErrorData, runConfiguration: RunConfiguration): Unit = {
+    runConfiguration.errorBehavior.handleError(e)
+  }
+
+  protected def doRun(input: Option[ProgramOutput]): JobOutput
+
+  def checks: Set[CheckFunction]
 }
 
 object Job {
   type JobOutput = Future[ProgramOutput]
+
+  type CheckFunction = ProgramOutput => Option[ErrorCause]
 }
