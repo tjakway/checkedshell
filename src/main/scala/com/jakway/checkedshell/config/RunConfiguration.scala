8fde9ef0f35ba6d9cc82ef43ee2803ad8e090c48
Thomas Jakway <tjakway@nyu.edu>
2019-08-18 14:41:36 -0400

    moved stream-related configuration to its own class

--- src/main/scala/com/jakway/checkedshell/config/RunConfiguration.scala
+++ src/main/scala/com/jakway/checkedshell/config/RunConfiguration.scala
@@ -6,20 +6,39 @@ import com.jakway.checkedshell.error.behavior.{ErrorBehavior, ThrowOnError}
 
 /**
  *
- * @param errorBehavior
- * @param charset
  * @param ignoreCloseStreamErrors
  * @param closeStreamsAfterExit whether write streams should be closed when a process exits
+ * @param closeStreamsOnReplacement whether replaced streams should be explicitly closed()
+ *                                  or just have their references reassigned
+ * @param closeOtherStreamsOnRedirect whether redirecting closes other streams for
+ *                                    that descriptor
+ */
+case class StreamsConfiguration(ignoreCloseStreamErrors: Boolean,
+                                closeStreamsAfterExit: Boolean,
+                                closeStreamsOnReplacement: Boolean,
+                                closeOtherStreamsOnRedirect: Boolean)
+
+object StreamsConfiguration {
+  val default: StreamsConfiguration =
+    StreamsConfiguration(
+      ignoreCloseStreamErrors = false,
+      closeStreamsAfterExit = true,
+      closeStreamsOnReplacement = true,
+      closeOtherStreamsOnRedirect = true)
+}
+
+/**
+ *
+ * @param errorBehavior
+ * @param charset
  */
 case class RunConfiguration(errorBehavior: ErrorBehavior,
                             charset: Charset,
-                            ignoreCloseStreamErrors: Boolean,
-                            closeStreamsAfterExit: Boolean)
+                            streamsConfiguration: StreamsConfiguration)
 
 object RunConfiguration {
   val default: RunConfiguration = RunConfiguration(
     ThrowOnError,
     StandardCharsets.UTF_8,
-    ignoreCloseStreamErrors = false,
-    closeStreamsAfterExit = true)
+    StreamsConfiguration.default)
 }
