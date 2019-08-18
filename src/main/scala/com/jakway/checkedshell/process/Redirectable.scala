21691c79e2e36bc0a61feabfcf50ed994d4607a3
Thomas Jakway <tjakway@nyu.edu>
2019-08-18 14:52:42 -0400

    implemented alterStreams

--- src/main/scala/com/jakway/checkedshell/process/Redirectable.scala
+++ src/main/scala/com/jakway/checkedshell/process/Redirectable.scala
@@ -1,7 +1,49 @@
 package com.jakway.checkedshell.process
 
-trait Redirectable[A] {
+import java.io.Writer
 
-  def alterStreams()
+import com.jakway.checkedshell.config.RunConfiguration
+import com.jakway.checkedshell.data.StreamWriters.StreamWriterMap
+import com.jakway.checkedshell.data.{HasStreamWriters, JobOutputStream, StreamWriters}
+
+trait Redirectable[A] extends HasStreamWriters[A] {
+
+  type AlterF = StreamWriterMap => JobOutputStream => Writer => StreamWriterMap
+
+  private def replaceF: AlterF = writerMap => descriptor => writer => {
+    writerMap.updated(descriptor, Seq(writer))
+  }
+
+  private def addF: AlterF = writerMap => descriptor => writer => {
+    val existingWriters: Seq[Writer] = writerMap.getOrElse(descriptor, Seq())
+    val newWriters: Seq[Writer] = existingWriters :+ writer
+    writerMap.updated(descriptor, newWriters)
+  }
+
+  /**
+   * normally we close streams when redirecting (hence why it's a redirect,
+   * not a tee) but closeOtherStreamsOnRedirect can be used to change this behavior
+   * @param rc
+   * @return
+   */
+  private def getAlterFunction(rc: RunConfiguration): AlterF = {
+    if(rc.streamsConfiguration.closeOtherStreamsOnRedirect) {
+      replaceF
+    } else {
+      addF
+    }
+  }
+
+  protected def alterStreams(newDescriptor: JobOutputStream,
+                             newWriter: Writer)
+                            (implicit rc: RunConfiguration): A = {
+    val sw = getStreamWriters
+    val alterF = getAlterFunction(rc)
+
+    val newWriterMap = alterF(sw.streamWritersMap)(newDescriptor)(newWriter)
+
+    val newSw = sw.copy(streamWritersMap = newWriterMap)
+    copyWithStreamWriters(newSw)
+  }
 
 }
