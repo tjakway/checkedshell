199890328e0b4b2f30b3ecae56df45e16ebc08e5
Thomas Jakway <tjakway@nyu.edu>
2019-08-21 16:10:36 -0400

    implemented Echo

--- src/main/scala/com/jakway/checkedshell/common/Echo.scala
+++ src/main/scala/com/jakway/checkedshell/common/Echo.scala
@@ -1,16 +1,36 @@
 package com.jakway.checkedshell.common
 
+import java.util.Formatter
+
 import com.jakway.checkedshell.config.RunConfiguration
 import com.jakway.checkedshell.data.{ProgramOutput, StreamWriters}
-import com.jakway.checkedshell.process.Job
-import com.jakway.checkedshell.process.Job.JobOutput
+import com.jakway.checkedshell.process.{Job, Task}
+import com.jakway.checkedshell.process.Job.{JobOutput, RunJobF}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-class Echo extends Job {
-  override protected def runJob(input: Option[ProgramOutput])(implicit rc: RunConfiguration, ec: ExecutionContext): JobOutput = ???
+class Echo(val printTrailingNewLine: Boolean)
+  extends Task {
+  override def runJob: RunJobF = Echo(printTrailingNewLine)
+}
 
-  override protected def getStreamWriters: StreamWriters = ???
+object Echo {
+  def apply(printTrailingNewLine: Boolean)
+           (optInput: Option[ProgramOutput])(rc: RunConfiguration,
+                                             ec: ExecutionContext): JobOutput =
+    Future {
+      val fmt: Formatter = new Formatter()
 
-  override protected def copyWithStreamWriters(newStreamWriters: StreamWriters): Job = ???
+      optInput.foreach { input =>
+        if(!input.stdout.isEmpty) {
+          fmt.format("%s", input.stdout)
+        }
+      }
+
+      if(printTrailingNewLine) {
+        fmt.format("\n")
+      } else {}
+
+      new ProgramOutput(0, toString, "")
+  }
 }
--- src/main/scala/com/jakway/checkedshell/process/Task.scala
+++ src/main/scala/com/jakway/checkedshell/process/Task.scala
@@ -2,12 +2,10 @@ package com.jakway.checkedshell.process
 
 import com.jakway.checkedshell.config.RunConfiguration
 import com.jakway.checkedshell.data.ProgramOutput
-import com.jakway.checkedshell.process.Job.JobOutput
+import com.jakway.checkedshell.process.Job.{JobOutput, RunJobF}
 
 import scala.concurrent.ExecutionContext
 
 abstract class Task {
-  def runJob(input: Option[ProgramOutput])
-            (implicit rc: RunConfiguration,
-             ec: ExecutionContext): JobOutput
+  def runJob: RunJobF
 }
