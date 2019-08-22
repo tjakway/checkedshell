2a4a7df54648b8fdbe9f2556054401de2956c44b
Thomas Jakway <tjakway@nyu.edu>
2019-08-22 17:53:24 -0400

    added line separator test

--- src/test/scala/com/jakway/checkshell/test/prop/EchoProperties.scala
+++ src/test/scala/com/jakway/checkshell/test/prop/EchoProperties.scala
@@ -2,6 +2,7 @@ package com.jakway.checkshell.test.prop
 
 import com.jakway.checkedshell.common.Echo
 import com.jakway.checkedshell.data.ProgramOutput
+import com.jakway.checkedshell.process.Job.JobOutput
 import com.jakway.checkedshell.process.TaskJob
 import com.jakway.checkedshell.test.framework.HasDefaultTestConfig
 import com.jakway.checkshell.test.framework.WithJobOutputMatcher
@@ -10,6 +11,8 @@ import org.scalatest.Matchers
 import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
+import scala.concurrent.Await
+
 class EchoProperties
   extends AnyPropSpec
     with HasDefaultTestConfig
@@ -17,14 +20,24 @@ class EchoProperties
     with ScalaCheckPropertyChecks
     with Matchers {
 
+  private def echoTest(e: Echo): JobOutput = {
+    TaskJob(e).run(None)
+  }
+
   property("flatMap pipes properly") {
     forAll(Gen.alphaNumStr) { (str: String) =>
-      val future = TaskJob(new Echo(false, Seq(str)))
-        .run(None)
-
       val expectedOutput = new ProgramOutput(0, str, "")
-      future should matchJobOutput(expectedOutput)
+      echoTest(new Echo(false, Seq(str))) should matchJobOutput(expectedOutput)
       //Await.result(future, getTestConfig.futureTimeOut).stdout shouldEqual str
+    }
+  }
+
+  property("prints with line separator") {
+    forAll(Gen.alphaNumStr) { (str: String) =>
+      val futureTestRes = echoTest(new Echo(true, Seq(str)))
+      val testRes: ProgramOutput = Await.result(futureTestRes, getTestConfig.futureTimeOut)
+
+      testRes.stdout.endsWith(System.lineSeparator()) shouldBe true
     }
   }
 }
