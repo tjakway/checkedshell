ed35ce8e85ab57843221a48c4657821046ab703d
Thomas Jakway <tjakway@nyu.edu>
2020-03-27 18:17:00 -0700

    Revert "WIP on runDefaultJob"
    This reverts commit df222442a25023b51440d690ee4bc25f16eeca74.

--- src/main/scala/com/jakway/checkedshell/config/DefaultJobBehavior.scala
+++ src/main/scala/com/jakway/checkedshell/config/DefaultJobBehavior.scala
@@ -1,17 +1,12 @@
 package com.jakway.checkedshell.config
 
 import com.jakway.checkedshell.data.output.FinishedProgramOutput
-import com.jakway.checkedshell.process.Job.{JobInput, RunJobF}
-import com.jakway.checkedshell.process.stream.pipes.output.OutputWrapper.{StderrWrapper, StdoutWrapper}
 import com.jakway.checkedshell.util.LogFunctions
 import org.slf4j.Logger
-
-import scala.concurrent.{ExecutionContext, Future}
 
 trait DefaultJobBehavior {
   protected def logF: LogFunctions.LogF
   protected def noInputMsg: String = DefaultJobBehavior.defaultNoInputMsg
-  def returnValue: Int
 
   def logNoInput: Logger => Unit = { (logger: Logger) =>
     logF(logger)(noInputMsg)
@@ -23,35 +18,16 @@ trait DefaultJobBehavior {
 
       logF(logger)(msg)
     }
-
-  def runDefaultJob(logger: Logger): RunJobF = {
-    (input: JobInput) => (stdout: StdoutWrapper) => (stderr: StderrWrapper) =>
-      (rc: RunConfiguration) => (ec: ExecutionContext) => {
-        input match {
-          case None => logNoInput(logger)
-          case Some(definedInput) => {
-            val writeStdoutFuture: Future[Unit] = Future {
-              definedInput.pipedStdout.toLines()
-                .foreach(log)
-            }(ec)
-          }
-        }
-      }
-  }
 }
 
 object DefaultJobBehavior {
   val default: DefaultJobBehavior =
     new LoggingDefaultJob(LogFunctions.debug)
 
-  val returnSuccess: Int = 0
-  def defaultReturnValue: Int = returnSuccess
-
   val defaultNoInputMsg: String = "DefaultJobBehavior received no input"
 
   class LoggingDefaultJob(val logF: LogFunctions.LogF,
-                          override val noInputMsg: String = defaultNoInputMsg,
-                          override val returnValue: Int = defaultReturnValue)
+                          override val noInputMsg: String = defaultNoInputMsg)
     extends DefaultJobBehavior
 
   object DoNothingJob extends LoggingDefaultJob(LogFunctions.doNothing)
