961b2804f17b466b5fcb6b90d2637c14e377534c
Thomas Jakway <tjakway@nyu.edu>
2020-04-03 16:38:05 -0700

    improved description handling

--- main/src/main/scala/com/jakway/checkedshell/error/checks/OutputCheckGroup.scala
+++ main/src/main/scala/com/jakway/checkedshell/error/checks/OutputCheckGroup.scala
@@ -1,12 +1,44 @@
 package com.jakway.checkedshell.error.checks
 
-case class OutputCheckGroup(taggedChecks: Seq[TaggedOutputChecks],
-                            optDescription: Option[String] = None)
+import java.util.Formatter
+
+case class OutputCheckGroup(taggedChecks: Seq[TaggedOutputChecks])
   extends ErrorCheckGroup[OutputCheck] {
 
   override def allChecks: Seq[OutputCheck] = {
     taggedChecks.foldLeft(Seq.empty: Seq[OutputCheck]) {
-      case (acc, TaggedOutputChecks(xs, _)) => acc ++ xs
+      case (acc, TaggedOutputChecks(xs, _, _)) => acc ++ xs
     }
+  }
+
+  private def formatDescription: String = {
+    val fmt = new Formatter(new StringBuffer())
+
+    fmt.format("{\n")
+    taggedChecks.foreach { thisCheck =>
+      fmt.format("\t%s", thisCheck.toString)
+    }
+    fmt.format("\n}")
+
+    fmt.toString.trim
+  }
+
+  override lazy val optDescription: Option[String] = {
+    if(taggedChecks.isEmpty) {
+      None
+    } else {
+      Some(formatDescription)
+    }
+  }
+
+  override def toString: String = {
+    val fmt = new Formatter(new StringBuffer())
+
+    fmt.format("%s(%s, %s)",
+      getClass.getName,
+      taggedChecks.toString(),
+      optDescription.getOrElse("< no description >"))
+
+    fmt.toString.trim
   }
 }
--- main/src/main/scala/com/jakway/checkedshell/error/checks/TaggedErrorChecks.scala
+++ main/src/main/scala/com/jakway/checkedshell/error/checks/TaggedErrorChecks.scala
@@ -3,7 +3,11 @@ package com.jakway.checkedshell.error.checks
 trait TaggedErrorChecks[A <: ErrorCheck] {
   def checks: Seq[A]
   def tag: CheckMode
+
+  def optDescription: Option[String]
 }
 
-case class TaggedOutputChecks(checks: Seq[OutputCheck], tag: CheckMode)
+case class TaggedOutputChecks(checks: Seq[OutputCheck],
+  tag: CheckMode,
+  optDescription: Option[String])
   extends TaggedErrorChecks[OutputCheck]
